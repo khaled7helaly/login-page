be48f59763db380adab1b6d65c75f52f7e7f1e61
khaled helaly <helalykhaled3@gmail.com>
2026-07-18 01:21:36 +0300

    Restore dark mode toggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import LoginPage from "./pages/LoginPage.jsx";
+import ThemeToggle from "./ThemeToggle.jsx";
 import "./App.css";
 
 function App() {
-  return <LoginPage />;
+  return (
+    <>
+      <div className="fixed right-6 top-6 z-50">
+        <ThemeToggle />
+      </div>
+
+      <LoginPage />
+    </>
+  );
 }
 
 export default App;
